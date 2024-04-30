e2bda40ef7e766be271cacb53f281a75f4b0dec1
Jorropo <jorropo.pgm@gmail.com>
2024-04-30 13:03:01 +0200

    bulk dedup in two stages
    This might or might not be an improvement, on my dataset (steam install folder with ~800GiB of games it didn't improved anything).
    I need to test wether or not btrfs is smart enough to dedup all references to an extent even tho it's reachable through many files already.

--- main.go
+++ main.go
@@ -5,22 +5,29 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"slices"
 	"sync"
+	"sync/atomic"
 
 	"golang.org/x/sys/unix"
 )
 
-var lk sync.Mutex
+var printLock sync.Mutex
 
 func print(s string) {
-	lk.Lock()
-	defer lk.Unlock()
+	printLock.Lock()
+	defer printLock.Unlock()
 	os.Stderr.WriteString(s)
 	os.Stderr.WriteString("\n")
 }
 
+type key struct {
+	hash   [sha256.Size]byte
+	length uint64 // to be recovered by [dedup], the hash would cover this otherwise.
+}
+
 var mlk sync.Mutex
-var m = make(map[[sha256.Size]byte]string)
+var m = make(map[key][]string)
 
 type kind uint8
 
@@ -142,61 +149,105 @@ func scan(p string) {
 		print(p + ": (WriteTo): " + err.Error())
 		return
 	}
-	sum := [sha256.Size]byte(h.Sum(nil))
+	sum := key{[sha256.Size]byte(h.Sum(nil)), length}
 
 	mlk.Lock()
-	target, ok := m[sum]
-	if !ok {
-		m[sum] = p
-		mlk.Unlock()
-		return
-	}
-	mlk.Unlock()
-	print("found dup: " + target + " " + p)
+	defer mlk.Unlock()
+	m[sum] = append(m[sum], p)
+}
 
-	tgt, err := os.Open(target)
-	if err != nil {
-		print(p + ": (target Open): " + err.Error())
-		return
-	}
-	defer tgt.Close()
+func dedup(backoff chan struct{}, length uint64, paths ...string) {
+	defer func() { <-backoff }()
 
-	fsc, err := f.SyscallConn()
-	if err != nil {
-		print(p + ": (SyscallConn): " + err.Error())
-		return
-	}
+	slices.Sort(paths) // FIXME: we need the first one for passing it to source, but do we need the other ones to be sorted ?
 
-	tsc, err := tgt.SyscallConn()
-	if err != nil {
-		print(p + ": (target SyscallConn): " + err.Error())
-		return
-	}
+	var release, filesAreReady, filesHaveAllExited sync.WaitGroup
 
-	var errr, errrr error
-	err = fsc.Control(func(ffd uintptr) {
-		errr = tsc.Control(func(tfd uintptr) {
-			errrr = unix.IoctlFileDedupeRange(int(tfd), &unix.FileDedupeRange{
-				Src_length: length,
-				Info:       []unix.FileDedupeRangeInfo{{Dest_fd: int64(ffd)}},
+	filesHaveAllExited.Add(len(paths))
+	defer filesHaveAllExited.Wait() // don't rely on scheduler fairness for bounding concurrency
+
+	release.Add(1)
+	defer release.Done()
+
+	filesAreReady.Add(len(paths))
+
+	var hasFailed atomic.Bool
+
+	dedups := make([]unix.FileDedupeRangeInfo, len(paths))
+	for i, p := range paths {
+		// Use a goroutine
+		go func() {
+			var good bool
+			defer func() {
+				if !good {
+					filesAreReady.Done()
+					hasFailed.Store(true)
+				}
+				filesHaveAllExited.Done()
+			}()
+
+			f, err := os.Open(p)
+			if err != nil {
+				print(p + ": (Open for dedup): " + err.Error())
+				return
+			}
+			defer f.Close()
+
+			fsc, err := f.SyscallConn()
+			if err != nil {
+				print(p + ": (SyscallConn): " + err.Error())
+				return
+			}
+
+			err = fsc.Control(func(fd uintptr) {
+				dedups[i].Dest_fd = int64(fd)
+				good = true
+				filesAreReady.Done()
+				release.Wait()
 			})
-		})
+			if err != nil {
+				print(p + ": (Control): " + err.Error())
+				return
+			}
+		}()
+	}
+
+	printLock.Lock()
+	os.Stderr.WriteString("found dups: ")
+	os.Stderr.WriteString(paths[0])
+	for _, p := range paths[1:] {
+		os.Stderr.WriteString("\n\t- ")
+		os.Stderr.WriteString(p)
+	}
+	os.Stderr.WriteString("\n")
+	printLock.Unlock()
+
+	filesAreReady.Wait()
+
+	valid := dedups[:]
+	for _, d := range dedups {
+		if d.Dest_fd == 0 {
+			continue
+		}
+		valid = append(valid, d)
+	}
+
+	if len(valid) < 2 {
+		return
+	}
+
+	err := unix.IoctlFileDedupeRange(int(valid[0].Dest_fd), &unix.FileDedupeRange{
+		Src_length: length,
+		Info:       valid[1:],
 	})
 	if err != nil {
-		print(p + ": (Control): " + err.Error())
-		return
-	}
-	if errr != nil {
-		print(p + ": (target Control): " + errr.Error())
-		return
-	}
-	if errrr != nil {
-		print(p + ": (IoctlFileDedupeRange): " + errrr.Error())
+		print(paths[0] + ": (FileDedupeRange): " + err.Error())
 		return
 	}
 }
 
 func main() {
+	// Run in two stages to prevent undeduplicating deduplicated content, first scan everything.
 	queue = []task{{kind: traversal, path: "."}}
 
 	concurrency := runtime.GOMAXPROCS(0) * 16
@@ -205,4 +256,20 @@ func main() {
 		go worker()
 	}
 	wg.Wait()
+
+	backoff := make(chan struct{}, concurrency)
+
+	// Then dedup duplicates, pass all files in bulk to FileDedupeRange.
+	for k, files := range m {
+		m[k] = nil
+		if len(files) < 2 {
+			continue
+		}
+		backoff <- struct{}{}
+		go dedup(backoff, k.length, files...)
+	}
+
+	for range concurrency {
+		backoff <- struct{}{}
+	}
 }
